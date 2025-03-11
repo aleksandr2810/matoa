650aa018661eefa2d54abeddd91368eb5c450af8
Alex <shcheglovalex1981@gmail.com>
2025-03-11 21:42:16 +0300

    first commit

---
+++ src/js/files/scroll/lazyload.js
@@ -0,0 +1,10 @@
+import LazyLoad from "vanilla-lazyload";
+
+const lazyMedia = new LazyLoad({
+	elements_selector: '[data-src],[data-srcset]',
+	class_loaded: '_lazy-loaded',
+	use_native: true
+});
+
+// Оновити модуль
+//lazyMedia.update();
